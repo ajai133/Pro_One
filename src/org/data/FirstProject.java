83f1a8532a3e2e5a07753c2fcdab213785d1132d
ajai133 <ajai133@gmail.com>
2022-06-19 12:57:27 +0530

    Sam has finished

--- src/org/data/FirstProject.java
+++ src/org/data/FirstProject.java
@@ -2,6 +2,11 @@ package org.data;
 
 public class FirstProject {
 
+	private void samWork() {
+		
+		System.out.println("I am Login");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("This is my first project");
