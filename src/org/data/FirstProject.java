ebb1a8d38af0105a7ac100a60888f5d2423ee16d
ajai133 <ajai133@gmail.com>
2022-06-19 13:03:10 +0530

    Vino has finished

--- src/org/data/FirstProject.java
+++ src/org/data/FirstProject.java
@@ -2,6 +2,11 @@ package org.data;
 
 public class FirstProject {
 
+	private void vinoWork() {
+		
+		System.out.println("I am submit button");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("This is my first project");
