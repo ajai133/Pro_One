3e3c4ce6e10a2538e8373f255a7ab514b9233878
ajai133 <ajai133@gmail.com>
2022-06-19 12:46:01 +0530

    Arun has finished

--- src/org/data/FirstProject.java
+++ src/org/data/FirstProject.java
@@ -2,6 +2,11 @@ package org.data;
 
 public class FirstProject {
 
+	private void arunWork() {
+		
+		System.out.println("I m email field");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("This is my first project");
